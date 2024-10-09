0fc942456eaa830dd771fb7bb798732067a064af
shahul <shahul.met@gmil.com>
2024-10-09 12:17:01 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #VPC
 resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr_block
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = var.dns_hostname
 
   tags = merge(
--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 #mandatory
-variable "vpc_cidr_block" {
-  default = "10.0.0.0/16"
+variable "vpc_cidr" {
+  # default = "10.0.0.0/16"
 }
 variable "dns_hostname" {
   default = true
